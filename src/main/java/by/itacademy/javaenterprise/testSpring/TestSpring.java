e868a98e26a90fe0a0be6d26a7039f166468833c
seledtsova <olgaseledtsova9@yandex.by>
2021-11-02 22:31:25 +0300

    add OrderService

--- src/main/java/by/itacademy/javaenterprise/testSpring/TestSpring.java
+++ src/main/java/by/itacademy/javaenterprise/testSpring/TestSpring.java
@@ -1,7 +1,8 @@
 package by.itacademy.javaenterprise.testSpring;
 
-import by.itacademy.javaenterprise.dao.CustomerService;
 import by.itacademy.javaenterprise.dao.impl.CustomerServiceImpl;
+import by.itacademy.javaenterprise.datasourse.ConnectionFactory;
+import by.itacademy.javaenterprise.datasourse.impl.ConnectionFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,10 +18,12 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        ConnectionFactory connectionFactory = context.getBean("ConnectionFactoryImpl", ConnectionFactoryImpl.class);
         CustomerServiceImpl customerServiceImpl = context.getBean("CustomerServiceImpl", CustomerServiceImpl.class);
         customerServiceImpl.deleteCustomerById(5);
         context.close();
     }
 }
+
 
 

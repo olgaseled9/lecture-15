5bc2aabc5c303d274beb641ade57d8109ee1bbf0
seledtsova <olgaseledtsova9@yandex.by>
2021-11-10 20:00:30 +0300

    add flyway, test

--- src/main/java/by/itacademy/javaenterprise/seledtsova/RunnerSpring.java
+++ src/main/java/by/itacademy/javaenterprise/seledtsova/RunnerSpring.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 
 public class RunnerSpring {
 
-    private final static Logger logger = LoggerFactory.getLogger(RunnerSpring.class);
+    private static final Logger logger = LoggerFactory.getLogger(RunnerSpring.class);
 
     public static void main(String[] args) throws SQLException {
 
